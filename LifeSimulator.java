f1a1d45a29407e99bdfb77850d2260d8b9cb8769
Nathan <barrosnathan105@gmail.com>
2026-09-21 20:17:31 -0500

    Overhauled logic for Interactive Update. New functions: showHome(), getFood(), getReligion()

--- LifeSimulator.java
+++ LifeSimulator.java
@@ -23,23 +23,63 @@ import java.util.Scanner;
 
 public class LifeSimulator {
 
+	private static Scanner userin = new Scanner(System.in);
+
 	/*
 	 * Prompts user to create a person then displays their bio
 	 */
     public static void main(String[] args) {
-        Utility.showBanner("q=quit");
+		String response;
+		Person p1 = Person("John", "Doe", "male", "christian", 37, 78.5, 178.5);
 
-        Person p1 = Person.createByUser(ESCAPE);
-		System.out.print("\n\n");
-        if (p1 == null) {
-            Utility.handleExit("Goodbye.", 0);
-        }
-        Person.showBioTable(p1, true);
-		System.out.print("\n\n[quit]");
-		String waitForUser = new Scanner(System.in).nextLine();
+		while (true) {
+			showHome();
 
-        Utility.handleExit("Goodbye.", 0);
+			response = userin.nextLine();
+			System.out.print("\n\n");
+
+			switch (response.toLowerCase()) {
+
+				case "v","1" -> Person.showBioTable(p1, true);
+				case "c","2" -> p1 = Person.createByUser();
+				case "g","3" -> p1.greet();
+				case "p","4" -> p1.pray();
+				case "f","5" -> p1.eat( getFood() );
+				case "s","6" -> p1.goToSleep();
+				case "e","7" -> p1.changeReligion( getReligion() );
+				default -> 
+					System.out.print("----- invalid selection -----\n[continue]");
+					userin.nextLine();
+			}
+		}
+
+		Utility.handleExit("Goodbye.", 0);
     }
+
+	private static String getFood() {
+		System.out.print("Enter any food: ");
+		return userin.nextLine();
+	}
+
+	private static String getReligion() {
+		System.out.print("Enter any religion: ");
+		return userin.nextLine();
+	}
+
+	private static showHome() {
+		Utility.showBanner("q=quit");
+		System.out.print(
+				"Options:\n"+
+				"    1. [V]iew Person"
+				"    2. [C]reate new Person\n"+
+				"    3. [G]reet\n"+
+				"    4. [P]ray\n"+
+				"    5. [F]eed\n"+
+				"    6. [S]leep\n+
+				"    7. [E]dit Religion\n\n"+
+
+				" : ");
+	}
 }
 
 // EOF
